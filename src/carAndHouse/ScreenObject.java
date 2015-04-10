c26fcb81fcfaf02be936f850b62b1a95cf4f9c21
juanvallejo <juan.vallejo.12@cnu.edu>
2015-04-10 02:01:36 -0400

    fix case-sensitive folder duplicate

--- src/carAndHouse/ScreenObject.java
+++ src/carAndHouse/ScreenObject.java
@@ -3,7 +3,6 @@ package carAndHouse;
 import javafx.scene.shape.Shape;
 
 public interface ScreenObject {
-	//why does this go to the wrong folder in git?
 	
 	public Shape getShape();
 	public String getType();
