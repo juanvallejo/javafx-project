8825ffd74b0047614ba929e42981abaffd2dfcb3
juanvallejo <juan.vallejo.12@cnu.edu>
2015-04-13 03:27:22 -0400

    redo GUIScreen; implement support for multiple scenes, cameras

--- src/carAndHouse/GUIScreen.java
+++ src/carAndHouse/GUIScreen.java
@@ -1,10 +1,9 @@
 package carAndHouse;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 import javafx.application.Application;
-import javafx.geometry.HPos;
 import javafx.geometry.Orientation;
 import javafx.geometry.Point3D;
 import javafx.scene.Camera;
@@ -13,167 +12,198 @@ import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.SubScene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.Node;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+/**
+ * UI Builder. Adds 3D cars and houses to the screen.
+ * TODO: Fix object rotation
+ * @author amalee, DooroftheTrog, juanvallejo
+ */
+
 public class GUIScreen extends Application {
 
-	public static final int SCREENWIDTH = 800;
-	public static final int SCREENHEIGHT = 600;
-	public int masterIndex = 0;
+	public final int SCREENWIDTH 	= 800;
+	public final int SCREENHEIGHT 	= 600;
 
-	List<car3D> cars;
-	List<house3D> houses;
-	List<ScreenObject> index;
+	public double mouseInitialX;
+	public double mouseInitialY;
 
-	public int getCar(){
-		for(int i=0; i<index.size(); i++){
-			index.get(i);
-			if(index.get(i).getType().equals("Car")){
+	public boolean dragHappened;
+
+	List<ScreenObject> objects;
+
+	/**
+	 * Remove the first object of type "Car" found on the list
+	 */
+	public int getFirstElementInstance(List<ScreenObject> list, String element) {
+
+		for(int i = 0; i < list.size(); i++){
+			if(list.get(i).getType().equals(element)){
 				return i;
 			}
 		}
-		return -1;
-	}
-	public int getHouse(){
-		for(int i=0; i<index.size(); i++){
-			index.get(i);
-			if(index.get(i).getType().equals("House")){
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	public void start(Stage primaryStage) {
 
-		// initialize list of cars and houses
-		cars 	= new ArrayList<car3D>();
-		houses 	= new ArrayList<house3D>();
-		index 	= new ArrayList<ScreenObject>();
+		return -1;
 
-		// Buttons do not expand past their natural size
-		FlowPane pane = new FlowPane(Orientation.VERTICAL);
-		FlowPane sub  = new FlowPane(Orientation.VERTICAL);
-		//pane.setColumnHalignment(HPos.LEFT);
-		pane.setMinSize(SCREENWIDTH, SCREENHEIGHT);
+	}
+
+	public void start(Stage stage) {
+
+		// initialize variables
+		objects 		= new ArrayList<ScreenObject>();
+
+		mouseInitialX 	= 0.0;
+		mouseInitialY 	= 0.0;
+
+		dragHappened 	= false;
+
+		// define 3D components
 		Camera camera = new PerspectiveCamera();
 		Group cameraGroup = new Group();
-		// getChildren() is a List
+
+		// define camera initial rotation state.
 		cameraGroup.getChildren().add(camera);
-		cameraGroup.setTranslateX(-SCREENWIDTH/2);
-		cameraGroup.setTranslateY(-SCREENHEIGHT);
-		cameraGroup.setTranslateZ(-1000);
-		// For rotating, later
-		cameraGroup.rotationAxisProperty().set(new Point3D(0, 1, 0));
+		cameraGroup.rotationAxisProperty().set(new Point3D(0, 0, 0));
 
-		Button carButton=new Button("Car");
-		Button houseButton=new Button ("House");
-		Button deleteCarButton=new Button("Delete Car");
-		Button deleteHouseButton=new Button ("Delete House");
-		Button helpButton=new Button("Help");
-		//pane.setLeft(carButton);
-		//pane.setRight(houseButton);//works for borderPane
-		
-		pane.getChildren().addAll(carButton, houseButton, deleteCarButton, deleteHouseButton,
-				helpButton);
 
-		masterIndex = pane.getChildren().size();
+		StackPane mainSceneLayout = new StackPane();
+		mainSceneLayout.setPrefSize(800, 600);
+		mainSceneLayout.setMinSize(800, 600);
 
-		// add new cars to the thingy
+		FlowPane buttonSceneLayout = new FlowPane(Orientation.VERTICAL);
+		buttonSceneLayout.setStyle("-fx-background-color: transparent");
+
+		FlowPane drawSceneLayout = new FlowPane(Orientation.VERTICAL);
+		drawSceneLayout.setStyle("-fx-background-color: transparent");
+
+		// create subscenes
+		SubScene buttonScene = new SubScene(buttonSceneLayout, 800, 600);
+		SubScene drawScene = new SubScene(drawSceneLayout, 800, 600);
+
+		// create subscene components
+		Button carButton 			= new Button("Car");
+		Button houseButton 			= new Button("House");
+		Button deleteCarButton 		= new Button("Delete Car");
+		Button deleteHouseButton 	= new Button("Delete House");
+		Button helpButton			= new Button("Help");
+
+		// set subscene properties
+		drawScene.setCamera(camera);
+
+		buttonScene.setOnMouseDragged(me -> {
+
+			dragHappened = true;
+
+			cameraGroup.translateZProperty().set((cameraGroup.getTranslateZ() - ((me.getY() - mouseInitialY) * 0.25)));
+			cameraGroup.rotateProperty().set(cameraGroup.rotateProperty().get() + ((me.getX() - mouseInitialX) * 20));
+
+		});
+
+		buttonScene.setOnMousePressed(me -> {
+			mouseInitialX = me.getX();
+			mouseInitialY = me.getY();
+		});
+
+		// add mouse events
+		buttonScene.setOnMouseReleased(me -> {
+
+			if(dragHappened) {
+				dragHappened = false;
+				return;
+			}
+
+		});
+
+		// add new cars to the pane
 		carButton.setOnAction(event -> {
 
-			//masterIndex++;
+				car3D car = new car3D();
+				objects.add(car);
 
-			car3D car = new car3D();
-			//car.setIndex(masterIndex);
-			cars.add(car);
-			index.add(car);
-
-			pane.getChildren().add(car.getShape());
+				drawSceneLayout.getChildren().add(car.getShape());
 
 		});
 
 		// add new houses to the pane
 		houseButton.setOnAction(event -> {
 
-			//masterIndex++;
-
 			house3D house = new house3D();
-			//house.setIndex(masterIndex);
-			houses.add(house);
-			index.add(house);
+			objects.add(house);
 
-			pane.getChildren().add(house.getShape());
+			drawSceneLayout.getChildren().add(house.getShape());
 
 		});
 
-		deleteCarButton.setOnAction(event ->{
-			if(cars.size() > 0) {
-				pane.getChildren().remove(masterIndex+getCar());
-				cars.remove(0);
-				index.remove(getCar());
-				//masterIndex--;
+		// delets the fist car on the object's list / from the screen
+		deleteCarButton.setOnAction(event -> {
+			
+			int elementIndex = getFirstElementInstance(objects, "Car");
+
+			if(objects.size() > 0 && elementIndex != -1) {
+				drawSceneLayout.getChildren().remove(elementIndex);
+				objects.remove(elementIndex);
 			}
+
 		});
 
+		// delets the fist house on the object's list / from the screen
 		deleteHouseButton.setOnAction(event->{
-			if(houses.size() > 0){
-				pane.getChildren().remove(masterIndex+getHouse());
-				houses.remove(0);
-				index.remove(getHouse());
-				//masterIndex--;
+				
+			int elementIndex = getFirstElementInstance(objects, "House");
+
+			if(objects.size() > 0 && elementIndex != -1) {
+				drawSceneLayout.getChildren().remove(elementIndex);
+				objects.remove(elementIndex);
 			}
+
 		});
+
 		helpButton.setOnAction(event -> {
 
 			final Stage dialog = new Stage();
-            dialog.initModality(Modality.APPLICATION_MODAL);
-            dialog.initOwner(primaryStage);
-            VBox dialogVbox = new VBox(20);
-            dialogVbox.getChildren().add(new Text("Help\nYou can add new cars with the 'Car'"
-            		+ " button, and add\n new houses with the 'House' button. The oldest house\n"
-            		+ " or car will be deleted when you select 'Delete House'\n or 'Delete Car'"
-            		+ " respectively. Click and drag the screen\n to rotate the camera. Camera"
-            		+ " will move opposite\n the direction of your mouse (ie up mouse = down \n"
-            		+ " camera.)"));
-            Scene dialogScene = new Scene(dialogVbox, 300, 200);
-            dialog.setScene(dialogScene);
-            dialog.show();
+			dialog.initModality(Modality.APPLICATION_MODAL);
+			dialog.initOwner(stage);
+
+			VBox dialogVbox = new VBox(20);
+			dialogVbox.getChildren().add(new Text("Help\nYou can add new cars with the 'Car'"
+				+ " button, and add\n new houses with the 'House' button. The oldest house\n"
+				+ " or car will be deleted when you select 'Delete House'\n or 'Delete Car'"
+				+ " respectively. Click and drag the screen\n to rotate the camera. Camera"
+				+ " will move opposite\n the direction of your mouse (ie up mouse = down \n"
+				+ " camera.)"
+			));
+
+			Scene dialogScene = new Scene(dialogVbox, 300, 200);
+			dialog.setScene(dialogScene);
+			dialog.show();
 
 		});
-		Group root = new Group(pane);
-		Scene scene = new Scene(root, SCREENWIDTH, SCREENHEIGHT);
 
-		//Scene scene2 = new Scene(sub, 200,300);
-		//root.setMouseTransparent(true);
-		//scene.setCamera(camera);
+		// add children to the button scene layout
+		buttonSceneLayout.getChildren().addAll(carButton, houseButton, deleteCarButton, deleteHouseButton, helpButton);
 
-		SubScene scene2 = new SubScene(sub, SCREENWIDTH,SCREENHEIGHT);
-		sub.setMouseTransparent(true);
+		// add children to the main scene layout
+		mainSceneLayout.getChildren().addAll(drawScene, buttonScene);
 
-		scene2.setCamera(camera);
-		scene2.setOnMouseDragged(me -> {
-			System.out.println("Camera dragged");
-			cameraGroup.translateZProperty().set(-1000 - (me.getY()*10) );
-			cameraGroup.rotateProperty().set((SCREENWIDTH/8 - (me.getX())/20));
-		});
+		// define main scene
+		Scene scene = new Scene(mainSceneLayout, 800, 600);
 
-		//helpButton needs to pop a dialog.
+		stage.setScene(scene);
+		stage.setTitle("My Title");
+		stage.show();
 
-		//Stage secondaryStage=new Stage();
-		//secondaryStage.setScene(scene2);
-		//secondaryStage.show();
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("Car and House");
-		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
